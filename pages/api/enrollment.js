40c334eaf85bda659e4ecdf5eb05a5c1e3bb57be
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-07-23 14:56:30 -0400

    get and post working

--- pages/api/enrollment.js
+++ pages/api/enrollment.js
@@ -1,8 +1,54 @@
-const db = require("../../server/models");
+const db = require("../../models/index.js");
 const Enrollment = db.enrollment;
 const Op = db.Sequelize.Op;
 
 export default (req, res) => {
+  switch (req.method) {
+    case "GET":
+      db.enrollment
+        .findByPk(req.body.enrollment_type)
+        .then((result) => res.json(result));
+
+      break;
+    case "POST":
+      if (!req.body) {
+        res.status(400).send({
+          message: "Content can not be empty!",
+        });
+        break;
+      }
+      console.log("i made it");
+      console.log(req.body);
+      // Create an enrollment
+      const enrollment = {
+        enrollment_type: req.body.enrollment_type,
+
+        // published: req.body.published ? req.body.published : false
+      };
+      // Save enrollment in the database
+      Enrollment.create(enrollment)
+        .then((data) => {
+          res.send(data);
+          res.json(data);
+        })
+        .catch((err) => {
+          res.status(500).send({
+            message:
+              err.message ||
+              "Some error occurred while creating the enrollment.",
+          });
+        });
+
+      break;
+    default:
+      res.status(405).end(); //Method Not Allowed
+      break;
+  }
+};
+
+/*
+export default (req, res) => {
+  consol;
   // Validate request
   if (!req.body.title) {
     console.log(req);
@@ -29,3 +75,4 @@ export default (req, res) => {
       });
     });
 };
+*/
--- server/models/Enrollment.js
+++ server/models/Enrollment.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING,
     },
   });
-
+  /*
   Enrollment.associate = (db) => {
     //enrollments belong to a course
     Enrollment.belongsTo(db.course, {
@@ -14,6 +14,8 @@ module.exports = (sequelize, Sequelize) => {
     Enrollment.belongsTo(db.users, {
       foreignKey: "userId",
     });
+    
   };
+*/
   return Enrollment;
 };
